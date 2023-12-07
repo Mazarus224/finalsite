c1a72f51c288599108b2eea6329aad5bc1aa7a4f
Mazarus224 <102100760+Mazarus224@users.noreply.github.com>
2023-12-07 11:22:50 -0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,47 @@
+
+
+function changeColor()
+{
+    var y1 = document.getElementById('y1');
+    var y2 = document.getElementById('y2');
+    var y3 = document.getElementById('y3');
+    var Color = '#37E007'; 
+    var Color2 = 'Gold'
+    y1.style.backgroundImage = 'linear-gradient(to right, gold 30%, #37E007 100%)';
+    y2.style.backgroundImage = 'linear-gradient(to top, gold 30%, #37E007 100%)';
+    y3.style.backgroundImage = 'linear-gradient(to top, gold 30%, #37E007 100%)';
+    y4.style.backgroundImage = 'linear-gradient(to left, gold 30%, #37E007 100%)';
+}
+
+function changeColorback()
+{
+    var y1 = document.getElementById('y1');
+    var y2 = document.getElementById('y2');
+    var y3 = document.getElementById('y3');
+    var Color = 'Gold'; 
+    var Color2 = '#37E007'
+    y1.style.backgroundImage = 'linear-gradient(to left, gold 30%, #37E007 100%)';
+    y2.style.backgroundImage = 'linear-gradient(to bottom, gold 30%, #37E007 100%)';
+    y3.style.backgroundImage = 'linear-gradient(to bottom, gold 30%, #37E007 100%)';
+    y4.style.backgroundImage = 'linear-gradient(to right, gold 30%, #37E007 100%)';
+}
+var count = 0;
+function like()
+{
+    var x = document.getElementById('x');
+    var Color = '#37E007'; 
+    count++;
+    x.style.backgroundColor = Color;
+    console.log(count);
+    window.alert(count)
+}
+var count1 = 0;
+function like1()
+{
+    var x1 = document.getElementById('x1');
+    var Color = '#37E007'; 
+    count1++;
+    x1.style.backgroundColor = Color;
+    console.log(count1);
+    window.alert(count1)
+}
